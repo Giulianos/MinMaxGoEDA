6ef891e769b311b7113536b3bc48733bcc64252b
fsammart <frperez@itba.edu.ar>
2017-06-19 13:41:24 -0300

    No se que paso, se me hizo un Wip o algo asi

--- src/main/java/group1/go/Controller/Controller.java
+++ src/main/java/group1/go/Controller/Controller.java
@@ -10,7 +10,7 @@ import group1.go.Model.MinMaxTree;
 import group1.go.Model.Move;
 import group1.go.Model.State;
 import group1.go.Model.heuristics.RandomHeuristic;
-import group1.go.Model.heuristics.TestHeuristic;import group1.go.Model.heuristics.TrivialHeuristic;
+import group1.go.Model.heuristics.TestHeuristic;import group1.go.Model.heuristics.TrivialHeuristic;import group1.go.Model.heuristics.squareHeuristic;
 
 public class Controller {
 
@@ -45,8 +45,19 @@ public class Controller {
 			game.add(i, j);
 			boardGUI.drawBoard(game.getState());
 			if(game.endTurn()){				boardGUI.endGame();			};
+<<<<<<< Updated upstream
 			if(isMachinePlaying){				machinePlay();			}			break;			
 		case Constants.KO:			boardGUI.KO();
+=======
+			MinMaxTree minMax=new MinMaxTree(game.getState(), game.getCurrentPlayer(), 2, new squareHeuristic());
+			Move m= minMax.getOptimalMoveBFS();			if(m.getPosition().getI()==-1){				if(game.pass()){					boardGUI.endGame();				}else{					return;				}			}
+			game.add(m.getPosition().getI(), m.getPosition().getJ());
+			boardGUI.drawBoard(game.getState());
+			if(game.endTurn()){
+				boardGUI.endGame();
+			};
+		case -4:
+>>>>>>> Stashed changes
 			break;
 		}
 	}	public void machinePlay() {				MinMaxTree minMax=new MinMaxTree(game.getState(), game.getCurrentPlayer(), machineDificult, new TestHeuristic());		Move m= minMax.getOptimalMoveBFS();		if(m.getPosition().getI()==-1){			if(game.pass()){				boardGUI.endGame();			}else{				return;			}		}		game.add(m.getPosition().getI(), m.getPosition().getJ());		boardGUI.drawBoard(game.getState());		if(game.endTurn()){			boardGUI.endGame();		}	}	private void playerPlay(int i, int j) {		game.add(i, j);		boardGUI.drawBoard(game.getState());		if(game.endTurn()){			boardGUI.endGame();		};			}
---
+++ src/main/java/group1/go/Model/heuristics/squareHeuristic.java
@@ -0,0 +1,32 @@
+package group1.go.Model.heuristics;
+
+import group1.go.Model.Board;
+import group1.go.Model.Constants;
+import group1.go.Model.Heuristic;
+import group1.go.Model.State;
+
+public class squareHeuristic implements Heuristic{
+	public int calculate(State s, char player) {
+		return calculate1(s, player) - calculate1(s, player==Constants.BLACK?Constants.WHITE:Constants.BLACK);
+	}
+
+	public int calculate1(State s, char player) {
+		int score=0;
+		char enemy= (player==Constants.BLACK)? Constants.WHITE: Constants.BLACK;
+		boolean foundPlayer=false;
+		for(int i=0; i<=Constants.BOARDSIZE; i++) {
+			for(int j=0; j<=Constants.BOARDSIZE; j++) {
+				if(s.getBoard().get(i, j)==player) {
+					score+=getSorround(s,i,j, player);
+				} 
+			}
+		}
+		return score;
+	}
+
+	private int getSorround(State s,int i, int j, char player) {
+		Board b= s.getBoard();
+		return ((b.get(i-1, j-1)==player)?1:0 )+ ((b.get(i+1, j+1)==player)?1:0) + ((b.get(i-1, j+1)==player)?1:0 )+((b.get(i+1, j-1)==player)?1:0);
+	}
+
+}
